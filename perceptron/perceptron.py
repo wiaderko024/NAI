d90a9e5029e26c9a0c0b3537a776493ca9924a8a
Kajetan Maślak <kajtek.maslak@icloud.com>
2022-03-28 17:53:56 +0200

    Deleted old not working project

--- perceptron/perceptron.py
+++
@@ -1,7 +0,0 @@
-import random
-
-
-class Perceptron:
-    def __init__(self, input_size):
-        self.w = [random.uniform(-1, 1) for i in range(0, input_size)]
-        self.t = random.uniform(-1, 1)
