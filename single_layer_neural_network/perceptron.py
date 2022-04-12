4dd69a6bffd757aaa6d5a602642df38f94118502
Kajetan Maślak <kajtek.maslak@icloud.com>
2022-04-12 14:56:07 +0200

    Fix

--- single_layer_neural_network/perceptron.py
+++ single_layer_neural_network/perceptron.py
@@ -3,7 +3,7 @@ import random
 
 class Perceptron:
     def __init__(self, input_size, alpha):
-        self.w = [random.uniform(-1, 1) for i in range(input_size)]
+        self.w = [random.uniform(-1, 1) for _ in range(input_size)]
         self.t = random.uniform(-1, 1)
         self.alpha = alpha
 
